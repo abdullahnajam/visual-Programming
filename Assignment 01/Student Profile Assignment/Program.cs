477af73939a4bbb8eec118c9b7fe6b018bfd2de5
abdullahnajam <anajam55@gmail.com>
2018-03-06 09:10:29 +0500

    - first

---
+++ Assignment 01/Student Profile Assignment/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Profile_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path;
+            if (args.Count() != 0)
+            {
+
+                path = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Path for file");
+                path = Console.ReadLine();
+            }
+            if (File.Exists(path) != true)
+            {
+                using (StreamWriter makefile = new StreamWriter(path))
+                {
+                    makefile.Close();
+                }
+                   
+            }
+
+
+            studentProfile obj = new studentProfile(path);
+            int id, sem; float cgpa; string name, dept, uni;
+            int choice, searchChoice, menuChoice;
+            Console.WriteLine("\t\t\t\"Student Profile System\"");
+            Console.WriteLine("\n\n\n\t\t\t1.Enter Record");
+            Console.WriteLine("\t\t\t2.Search Student Record");
+            Console.WriteLine("\t\t\t3.Delete Student Record");
+            Console.WriteLine("\t\t\t4.Get Top 3 Students");
+            Console.WriteLine("\t\t\t5.Mark Attendance");
+            Console.WriteLine("\t\t\t6.View Attendance");
+            choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+
+                do
+                {
+
+                    Console.WriteLine("Enter Student ID");
+                    id = int.Parse(Console.ReadLine());
+                    if (obj.searchbyID(id) == true)
+                    {
+                        Console.WriteLine("Id already taken");
+                    }
+                }
+                while (obj.searchbyID(id) == true);
+                Console.WriteLine("Enter Student Name");
+                name = Console.ReadLine();
+                do
+                {
+
+                    Console.WriteLine("Enter Student Semester");
+                    sem = int.Parse(Console.ReadLine());
+                    if (sem > 8 || sem < 1)
+                    {
+                        Console.WriteLine("Semester must be from 1-8.Please enter Again");
+                    }
+                }
+                while (sem < 1 || sem > 8);
+                do
+                {
+
+                    Console.WriteLine("Enter Student CGPA");
+                    cgpa = float.Parse(Console.ReadLine());
+                    if (cgpa > 4 || cgpa < 0)
+                    {
+                        Console.WriteLine("CGPA must be from 0.0-4.0.Please enter Again");
+                    }
+                }
+                while (cgpa < 1 || cgpa > 4);
+
+                Console.WriteLine("Enter Student Department");
+                dept = Console.ReadLine();
+                Console.WriteLine("Enter Student University");
+                uni = Console.ReadLine();
+                obj.CreateProfile(path,id, name, sem, cgpa, dept, uni);
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("\n\n\n\t\t\t1.Search by ID");
+
+                Console.WriteLine("\t\t\t2.Search by Name");
+                Console.WriteLine("\t\t\t3.Search by Semester");
+                searchChoice = int.Parse(Console.ReadLine());
+                if (searchChoice == 1)
+                {
+                    do
+                    {
+                        Console.WriteLine("Enter Student ID");
+                        id = int.Parse(Console.ReadLine());
+                        if (obj.searchbyID(id) == false)
+                        {
+                            Console.WriteLine("\t\t\tno record found please try again");
+                        }
+                        else
+                            obj.searchStudent(id);
+
+                    }
+                    while (obj.searchbyID(id) == false);
+                }
+                else if (searchChoice == 2)
+                {
+                    do
+                    {
+                        Console.WriteLine("Enter Student Name");
+                        name = Console.ReadLine();
+                        if (obj.searchbyStudent(name) == false)
+                        {
+                            Console.WriteLine("\t\t\tno record found please try again");
+                        }
+                        else
+                            obj.searchStudent(name);
+
+                    }
+                    while (obj.searchbyStudent(name) == false);
+                }
+                else if (searchChoice == 3)
+                {
+                    do
+                    {
+                        Console.WriteLine("Enter Student Semester");
+                        sem = int.Parse(Console.ReadLine());
+                        if (obj.searchbySemester(sem) == false)
+                        {
+                            Console.WriteLine("\t\t\tno record found please try again");
+                        }
+                        else
+                            obj.searchSemester(sem);
+
+                    }
+                    while (obj.searchbySemester(sem) == false);
+                }
+            }
+            else if (choice == 3)
+            {
+                do
+                {
+
+                    Console.WriteLine("Enter Student ID");
+                    id = int.Parse(Console.ReadLine());
+                    if (obj.searchbyID(id) == false)
+                    {
+                        Console.WriteLine("Id doesnot Exists");
+                    }
+                    else
+                        obj.deleteRecord(id);
+                }
+                while (obj.searchbyID(id) == false);
+            }
+            else if (choice == 4)
+            {
+                obj.getTop3();
+            }
+            else if (choice == 5)
+            {
+                do
+                {
+
+                    Console.WriteLine("Enter Student Semester");
+                    sem = int.Parse(Console.ReadLine());
+                    if (sem > 8 || sem < 1)
+                    {
+                        Console.WriteLine("Semester must be from 1-8.Please enter Again");
+                    }
+                    else
+                        obj.markAttendence(sem);
+                }
+                while (sem < 1 || sem > 8);
+            }
+            else if (choice == 6)
+            {
+                do
+                {
+
+                    Console.WriteLine("Enter Student Semester");
+                    sem = int.Parse(Console.ReadLine());
+                    if (sem > 8 || sem < 1)
+                    {
+                        Console.WriteLine("Semester must be from 1-8.Please enter Again");
+                    }
+                    else
+                        obj.viewAttendance(sem);
+                }
+                while (sem < 1 || sem > 8);
+            }
+            else
+            {
+                Console.WriteLine("invalid Choice");
+            }
+
+
+        }
+    }
+    
+}
---
+++ Assignment 01/Student Profile Assignment/studentProfile.cs
@@ -0,0 +1,429 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Profile_Assignment
+{
+    class studentProfile
+    {
+        string path;
+        
+        string [] storeHead={"ID : ","Name : ","Semester : ","CPGA : ","Department : ","University : "} ;
+        public studentProfile(string filepath)
+        {
+            path = filepath;
+        }
+        public void CreateProfile(string p,int id, string name, int sem, float cgpa, string dept, string uni)
+        {
+            try
+            {
+                using (StreamWriter write = File.AppendText(p))
+                {
+                    write.WriteLine(id);
+                    write.WriteLine(name);
+                    write.WriteLine(sem);
+                    write.WriteLine(cgpa);
+                    write.WriteLine(dept);
+                    write.WriteLine(uni);
+                    write.WriteLine("f");
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("File not found");
+                    File.Create(path);
+            }
+            
+        }
+        public bool searchStudent(int id)
+        {
+            
+            string[] store_data = new String[7];
+            bool ack = false;
+            using (StreamReader r = new StreamReader(path))
+            {
+                while (!r.EndOfStream)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        store_data[i] = r.ReadLine();
+                    }
+
+
+                    if (store_data[0] == id.ToString())
+                    {
+                        ack = true;
+                        for (int i = 0; i < 6; i++)
+                        {
+                            Console.Write(storeHead[i]);
+                            Console.WriteLine(store_data[i]);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 7; i++)
+                        {
+                            store_data[i] = null;
+                        }
+                    }
+                }
+            }
+                
+            
+            return ack;
+            
+        }
+        public bool searchbyID(int id)
+        {
+            string[] store_data = new String[7]; bool rtype = false;
+            using (StreamReader r = new StreamReader(path))
+            {
+                while (!r.EndOfStream)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        store_data[i] = r.ReadLine();
+                    }
+
+
+                    if (store_data[0] == id.ToString())
+                    {
+                        rtype = true;
+
+                        break;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 7; i++)
+                        {
+                            store_data[i] = null;
+                        }
+                    }
+                }
+            }
+                
+            return rtype;
+        }
+
+
+        public void searchStudent(string name)
+        {
+            
+
+            string[] store_data = new String[7];
+            using (StreamReader r = new StreamReader(path))
+            {
+                while (!r.EndOfStream)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        store_data[i] = r.ReadLine();
+                    }
+
+
+                    if (store_data[1] == name)
+                    {
+
+                        for (int i = 0; i < 6; i++)
+                        {
+                            Console.Write(storeHead[i]);
+                            Console.WriteLine(store_data[i]);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 7; i++)
+                        {
+                            store_data[i] = null;
+                        }
+                    }
+                }
+            }
+                
+        }
+        public bool searchbyStudent(string name)
+        {
+            bool ack = false;
+
+            string[] store_data = new String[7];
+            using (StreamReader r = new StreamReader(path))
+            {
+                while (!r.EndOfStream)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        store_data[i] = r.ReadLine();
+                    }
+
+
+                    if (store_data[1] == name)
+                    {
+                        ack = true;
+                        for (int i = 0; i < 6; i++)
+                        {
+                            Console.Write(storeHead[i]);
+                            Console.WriteLine(store_data[i]);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 7; i++)
+                        {
+                            store_data[i] = null;
+                        }
+                    }
+                }
+            }
+                
+            return ack;
+        }
+        public bool searchbySemester(int sem)
+        {
+            bool ack=false;
+            string[] store_data = new String[7];
+            using (StreamReader r = new StreamReader(path))
+            {
+                while (!r.EndOfStream)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        store_data[i] = r.ReadLine();
+                    }
+                    if (store_data[2] == sem.ToString())
+                    {
+                        ack = true;
+                        for (int i = 0; i < 6; i++)
+                        {
+                            Console.Write(storeHead[i]);
+                            Console.WriteLine(store_data[i]);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 7; i++)
+                        {
+                            store_data[i] = null;
+                        }
+                    }
+                }
+            }
+                
+            return ack;
+        }
+        public void searchSemester(int sem)
+        {
+            
+            string[] store_data = new String[7];
+            using (StreamReader r = new StreamReader(path))
+            {
+                while (!r.EndOfStream)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        store_data[i] = r.ReadLine();
+                    }
+                    if (store_data[2] == sem.ToString())
+                    {
+
+                        for (int i = 0; i < 6; i++)
+                        {
+                            Console.Write(storeHead[i]);
+                            Console.WriteLine(store_data[i]);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 7; i++)
+                        {
+                            store_data[i] = null;
+                        }
+                    }
+                }
+            }
+                
+        }
+        public int recordlist()
+        {
+            string line;
+            int count = 0;
+            StreamReader r = new StreamReader(path);
+            while ((line = r.ReadLine()) != null)
+            {
+                if (line != "")
+                    count++;
+            }
+            return count / 7;
+        }
+        public void getTop3()
+        {
+            string[] store_data = new String[recordlist()];
+            string line;
+            int j = 0;
+            StreamReader r = new StreamReader(path);
+            while ((line = r.ReadLine()) != null)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    line = r.ReadLine();
+                    if (i == 2)
+                    {
+                        store_data[j] = line;
+                        j++;
+                    }
+                }
+            }
+            Array.Sort(store_data);
+            try
+            {
+                for (int i = 2; i >= 0; i--)
+                {
+                    Console.WriteLine(store_data[i]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("less than 3 records");
+            }
+            
+
+        }
+        public void deleteRecord(int id)
+        {
+            string line;
+            int count = 0;
+            StreamReader r = new StreamReader(path);
+            while ((line = r.ReadLine()) != null)
+            {
+                count++;
+            }
+            r.Close();
+            r = new StreamReader(path);
+            string[] store_data = new String[count];
+            for (int i = 0; i < count; i++)
+            {
+
+                store_data[i] = r.ReadLine();
+                if (store_data[i] == id.ToString())
+                {
+                    //store_data[i] = null;
+                    i--;
+                    for (int j = 0; j < 6; j++)
+                    {
+                        line = r.ReadLine();
+                    }
+                }
+            }
+            r.Close();
+            using (StreamWriter write = new StreamWriter(path))
+            {
+                for (int i = 0; i < store_data.Length; i++)
+                {
+                    write.WriteLine(store_data[i]);
+                }
+            }
+
+
+
+        }
+
+        
+        
+        public void markAttendence(int semester)
+        {
+            
+            string[] store_data = new String[7];
+            string line;
+            int count = 0,inc=0;
+            using (StreamReader r = new StreamReader(path))
+            {
+                while ((line = r.ReadLine()) != null)
+                {
+                    count++;
+                }
+                
+            }
+            string[] storeRecord = new String[count];
+            using (StreamReader r = new StreamReader(path))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    storeRecord[i] = r.ReadLine();
+                }
+
+                for (int i = 0; i < storeRecord.Length; i++)
+                {
+                    inc = i;
+                    for (int j = 0; j < 7; j++)
+                    {
+                        store_data[j] = storeRecord[i];
+                        i++;
+                    }
+                    i--;
+                    if (store_data[2] == semester.ToString())
+                    {
+                        Console.Write(store_data[0] + "  ");
+                        Console.Write(store_data[1] + "  : ");
+                        storeRecord[i] = Console.ReadLine();
+
+                    }
+
+
+
+                }
+            }
+                
+            
+            using (StreamWriter w = new StreamWriter(path))
+            {
+                for (int i = 0; i < storeRecord.Length; i++)
+                {
+                    w.WriteLine(storeRecord[i]);
+
+                }
+                
+            }
+            
+            
+
+        }
+        public void viewAttendance(int semester)
+        {
+            string[] store_data = new String[7];
+            string line;
+            using (StreamReader r = new StreamReader(path))
+            {
+                while(!r.EndOfStream)
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        store_data[i] = r.ReadLine();
+                    }
+                    if(store_data[2]==semester.ToString())
+                    {
+                        Console.Write(store_data[0] + "  ");
+                        Console.Write(store_data[1] + "  : ");
+                        Console.WriteLine(store_data[6]);
+                    }
+                }
+                
+            }
+        }
+        
+    }
+}
+        
+            
+            
+
+        
+
+
+    
+
